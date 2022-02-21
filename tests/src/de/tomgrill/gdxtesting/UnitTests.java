28b10dbefb99c50d0f3ad5cc52ccc360bba1a4bb
Harsh Mohan <harshmohan813@gmail.com>
2022-02-21 01:04:09 +0000

    Add unit test support

---
+++ tests/src/de/tomgrill/gdxtesting/UnitTests.java
@@ -0,0 +1,12 @@
+package de.tomgrill.gdxtesting;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class UnitTests {
+
+    @Test
+    public void oneEqualsOne() {
+        assertEquals(1, 1);
+    }
+}
